af6f20b172f5267deb634c08ac8943c229c61b19
Ian Dawes <ian@dawesnet.net>
2015-03-14 21:11:01 -0400

    Moving comment so that it doesn't get interpreted as package comment

--- cmd/constants.go
+++ cmd/constants.go
@@ -1,5 +1,6 @@
-// Constants used in multiple places in the app
 package cmd
+
+// Constants used in multiple places in the app
 
 const (
 	// apiUrl string = "http://api.golang-challenge.dev/v1/challenges"
