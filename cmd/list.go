737ffa68a04fa9c8bee611f8945b8ca36e8530c3
Ian Dawes <ian@dawesnet.net>
2015-03-15 23:53:46 -0400

    Removing redundant code

--- cmd/list.go
+++ cmd/list.go
@@ -14,9 +14,6 @@ func List(c *cli.Context) {
 		fmt.Println(err)
 		return
 	}
-	if len(challenges) == 0 {
-		fmt.Println("Found no challenges")
-	}
 	w := tabwriter.NewWriter(os.Stdout, 0, 4, 0, '\t', 0)
 	for _, chal := range challenges {
 		fmt.Fprintf(w, "%s\t(%s)\t   ---  \tid: %d\n", chal.Name, chal.Status, chal.ID)
